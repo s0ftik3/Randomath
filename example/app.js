763004049778590c285ce674cd08939e8e950276
s0ftik3 <slava021323@gmail.com>
2020-09-01 23:46:09 +0600

    Minor fixes.

--- example/app.js
+++ example/app.js
@@ -1,17 +1,17 @@
 const Randomath = require('../src/randomath');
 const math = new Randomath();
 
-// // getRandomAdd method example
-// console.log(math.getRandomAdd());
+// getRandomAdd method example
+console.log(math.getRandomAdd());
 
-// // getRandomSubtract method example
-// console.log(math.getRandomSubtract());
+// getRandomSubtract method example
+console.log(math.getRandomSubtract());
 
-// // getRandomMultiply method example
-// console.log(math.getRandomMultiply());
+// getRandomMultiply method example
+console.log(math.getRandomMultiply());
 
-// // getRandomDivide method example
-// console.log(math.getRandomDivide());
+// getRandomDivide method example
+console.log(math.getRandomDivide());
 
 // getRandomCompare method example
 console.log(math.getRandomCompare());
